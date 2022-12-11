d481dc5879d2ef5cbee1a39c9331ae9f55b48239
Noner <spcolin@192.168.0.101>
2022-12-11 15:59:01 +0800

    color map ref link added

--- depth_vis.py
+++ depth_vis.py
@@ -1,6 +1,14 @@
+"""
+find suitable color map from: https://matplotlib.org/2.0.2/examples/color/colormaps_reference.html
+"""
+
+
 import matplotlib.pyplot as plt
 
 img=plt.imread('depth.png')
 
 plt.imshow(img, cmap='jet')
 plt.show()
+
+
+
